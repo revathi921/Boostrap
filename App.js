2a39b795d864d86482d98644902c23539a6e4103
revathi921 <111131515+revathi921@users.noreply.github.com>
2022-08-13 11:27:49 +0530

    signup form

--- App.js
+++ App.js
@@ -1,28 +1,37 @@
 import React,{useState} from 'react'
 
 const App = () => {
-  const[data,setData]=useState({
-    username:'',
-    password:''
-  })
-  const {username,password}=data;
+const [data,setData]=useState({
+  username:'',
+  email:'',
+  password:'',
+  confrimPassword:''
+})
+  const {username,email,password,confrimPassword}=data;
   const changeHandler=e=>{
     setData({...data,[e.target.name]:[e.target.value]})
   }
   const submitHandler=e=>{
-    e.preventDefault()
-    console.log(data);
+    e.preventDefault();
+    if(password==confrimPassword){
+      alert("password and confrim password should be same")
+    }
+    else{
+      console.log("password and confrimPassword should be same")
+    }
   }
+
   return (
     <div>
-      <center>
+      
         <form onSubmit={submitHandler}>
-          Username:<input type="text" name="username" placeholder='username or email address' value={username} onChange={changeHandler}/><br></br>
-          Password:<input type="password" name="password" placeholder='password' value={password} onChange={changeHandler}/><br></br>
+        Username:<input type="text" name="username" value={username} onChange={changeHandler}/><br></br>
+        Email:<input type="email" name="email" value={email} onChange={changeHandler}/><br></br>
+        Password:<input type="password" name="password" value={password} onChange={changeHandler}/><br></br>
+        Confrim Password:<input type="password" name="confrimPassword" value={confrimPassword} onChange={changeHandler}/><br></br>
           <input type="submit" name="submit"/>
-       
         </form>
-      </center>
+      
     </div>
   )
 }
